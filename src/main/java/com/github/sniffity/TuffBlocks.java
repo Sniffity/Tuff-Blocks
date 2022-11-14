abd1d2116b986b3dee81ce2980fd74cc68604574
Sniffity <66455728+Sniffity@users.noreply.github.com>
2022-11-13 20:30:22 -0500

    Port to 1.18

--- src/main/java/com/github/sniffity/TuffBlocks.java
+++ src/main/java/com/github/sniffity/TuffBlocks.java
@@ -12,7 +12,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fmllegacy.RegistryObject;
+import net.minecraftforge.registries.RegistryObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
