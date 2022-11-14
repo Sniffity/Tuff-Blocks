f88b0d0d0e05743c4af563b5beed105f3978e203
Sniffity <66455728+Sniffity@users.noreply.github.com>
2022-11-13 20:57:14 -0500

    Port to 1.19

--- src/main/java/com/github/sniffity/TuffBlocks.java
+++ src/main/java/com/github/sniffity/TuffBlocks.java
@@ -7,12 +7,10 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import  net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -29,22 +27,4 @@ public class TuffBlocks {
 
         MinecraftForge.EVENT_BUS.register(this);
     }
-
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
-    }
-
-    @SubscribeEvent
-    public static void onRegisterItems(final RegistryEvent.Register<Item> event) {
-        TuffBlocksRegistry.BLOCKS.getEntries().stream().map(RegistryObject::get).forEach(block -> {
-            event.getRegistry().register(new BlockItem(block, new Item.Properties().tab(TuffItemGroupRegistry.GROUP))
-                    .setRegistryName(block.getRegistryName()));
-        });
-    }
-
 }
